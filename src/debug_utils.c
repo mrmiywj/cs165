963915bb4eea86fc04640bd04ce9af3d1dbd40aa
Kevin Zhang <kevin.zhang500@gmail.com>
2016-10-25 15:06:57 -0400

    Debug: converted DbOperator print to use log_info instead of printf

--- src/debug_utils.c
+++ src/debug_utils.c
@@ -4,46 +4,46 @@
 void printDbOperator(DbOperator* query) {
     #ifdef LOG_INFO
         if (query == NULL) {
-            printf("<null DbOperator object>\n");
+            log_info("<null DbOperator object>\n");
             return;
         }
 
         /* DbOperator.client_fd */
-        printf("Client FD: %i\n", query->client_fd);
+        log_info("Client FD: %i\n", query->client_fd);
 
         OperatorFields fields = query->operator_fields;
         /* DbOperator.type */
         switch (query->type) {
             case CREATE:
-                printf("Create:\n");
+                log_info("Create:\n");
                 break;
             case INSERT:
-                printf("Insert:\n");
+                log_info("Insert:\n");
                 /* DbOperator.operator_fields.insert_operator.table.name */
                 if (fields.insert_operator.table == NULL) {
-                    printf("\tInsert: No table object\n");
+                    log_info("\tInsert: No table object\n");
                 } else {
-                    printf("\tInsert table: %s\n", fields.insert_operator.table->name);
-                    printf("\t            : %i columns\n", fields.insert_operator.table->col_count);
-                    printf("\t            : %i table length\n", fields.insert_operator.table->table_length);
+                    log_info("\tInsert table: %s\n", fields.insert_operator.table->name);
+                    log_info("\t            : %i columns\n", fields.insert_operator.table->col_count);
+                    log_info("\t            : %i table length\n", fields.insert_operator.table->table_length);
                 }
 
                 /* DbOperator.operator_fields.insert_operator.values */
-                printf("\tInsert Values: [ ");
+                log_info("\tInsert Values: [ ");
                 if (fields.insert_operator.values != NULL) {
                     for (int i = 0; i < sizeof(fields.insert_operator.values) / sizeof(int); i++) {
-                        printf("%i ", fields.insert_operator.values[i]);
+                        log_info("%i ", fields.insert_operator.values[i]);
                     }
                 }
-                printf("]\n");
+                log_info("]\n");
                 break;
             case OPEN:
-                printf("Open:\n");
+                log_info("Open:\n");
                 /* DbOperator.operator_fields.open_operator.db_name */
                 if (fields.open_operator.db_name == NULL) {
-                    printf("\tOpen: No database name\n");
+                    log_info("\tOpen: No database name\n");
                 } else {
-                    printf("\tOpen: database %s\n", fields.open_operator.db_name);
+                    log_info("\tOpen: database %s\n", fields.open_operator.db_name);
                 }
                 break;
             default:
@@ -51,10 +51,12 @@ void printDbOperator(DbOperator* query) {
         }
 
         /* DbOperator.context */
-        // printf("Context: ");
-        // printf("         %i handles in use\n", query->context->chandles_in_use);
-        // printf("         %i handle slots\n", query->context->chandle_slots);
+        // log_info("Context: ");
+        // log_info("         %i handles in use\n", query->context->chandles_in_use);
+        // log_info("         %i handle slots\n", query->context->chandle_slots);
 
-        printf("\n");
+        log_info("\n");
+    #else 
+        (void) query;
     #endif
 }
